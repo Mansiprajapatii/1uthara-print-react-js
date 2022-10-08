87851420a6665a4520ed23648da42554dd505bdc
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2022-10-08 23:42:55 +0530

    second commit

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,40 +5,14 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 // import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 export default function footer() {
   return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
-        </div>
-    
-        <div>
+    <MDBFooter  className='text-center text-lg-start text-muted' >
 
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
-          </a>
-        </div>
-      </section>
 
-      <section className=''>
+      <section className='' style={{MarginTop: 50,backgroundColor: "#c2bfbe"}}    >
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
             <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h4 className='text-uppercase fw-bold mb-4'>
+              <h4 className='text-uppercase fw-bold mb-4 mt-5'>
                 <MDBIcon icon="gem" className="me-3" />
                 Uthara print
               </h4>
@@ -49,56 +23,71 @@ export default function footer() {
               </p>
             </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+            <MDBCol md="2" lg="1" xl="1" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4 mt-5'>Products</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Angular
+                <a >
+                  Office
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  React
+                <a >
+                  Marketing
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
+                <a >
+                  Promotion
+                </a>
+              </p>
+              <p>
+                <a >
+                  Display
+                </a>
+              </p>
+              <p>
+                <a >
+                  Books
+                </a>
+              </p>
+              <p>
+                <a >
                   Vue
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
+                <a >
                   Laravel
                 </a>
               </p>
             </MDBCol>
 
             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <h6 className='text-uppercase fw-bold mb-4 mt-5'>Useful links</h6>
               <p>
-                <a href='#!' className='text-reset'>
+                <a >
                   Pricing
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
+                <a >
                   Settings
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
+                <a >
                   Orders
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Help
-                </a>
+                <a> Help</a>
+                 
+                
               </p>
             </MDBCol>
 
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <h6 className='text-uppercase fw-bold mb-4 mt-5'>Contact</h6>
               <p>
                 <MDBIcon icon="home" className="me-2" />
                 New York, NY 10012, US
